66df1105ec3526e16a1639a7c053f4f7136679eb
theitproinfo <theitproinfo@gmail.com>
2022-04-04 21:37:19 -0400

    Add project files.

---
+++ day7ClassDemo2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7ClassDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student tom = new Student("Tom",26,"male");
+            //tom.Name = "Tom";
+            //tom.Age = 26;
+            //tom.Gender = "male";
+
+            tom.SayHello();
+            Student.Say2();
+
+            Student jerry = new Student("Jerry",32,"male");
+            //jerry.Name = "jerry";
+            //jerry.Age = 32;
+            //jerry.Gender = "spring";
+            jerry.SayHello();
+
+            Student alice = new Student("Alice","Female");
+            //alice.Name = "Alice";
+            //alice.Age = 25;
+            //alice.Gender = "Female";
+            alice.SayHello();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ day7ClassDemo2/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7ClassDemo2
+{
+      class Student
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private int _age;
+        public int Age
+        {
+            set { _age = value; }
+            get { return _age; }
+        }
+
+        private string _gender;
+
+        public string Gender
+        {
+           set { _gender=value;}
+            get { return _gender; }
+          }
+
+        public void SayHello()
+        {
+            Console.WriteLine("I am {0}, {1}years old, {2}",this.Name,this.Age,this.Gender);
+        }
+
+        public static void Say2()
+        {
+            Console.WriteLine("this is static method");
+        }
+        
+        public Student(string name,int age, string gender) // constructor method
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public  Student(string name,string gender)
+        {
+            this.Name = name;
+            this.Gender = gender;
+        }
+
+        public Student()
+        { 
+        }
+    
+    }
+}
